0f78b6d9361023223162d6f89da879fa230b1962
Ivan Baksheev <zpix-dev@list.ru>
2023-03-22 22:02:21 +0700

    Added title link

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import { ReactNode } from "react";
 
 export const Base = ({ children }: { children: ReactNode }) => {
@@ -7,7 +8,9 @@ export const Base = ({ children }: { children: ReactNode }) => {
       <Head>
         <title>DEX Arbitrageur</title>
       </Head>
-      <div className="center pt-5 text-center text-2xl">DEX Arbitrageur</div>
+      <div className="center pt-5 text-center text-2xl">
+        <Link href="/">DEX Arbitrageur</Link>
+      </div>
       <main className="m-auto mt-5 max-w-6xl rounded bg-slate-300 p-5">
         {children}
       </main>
