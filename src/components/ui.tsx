1651d464e083006f967a4b2193737c687834f634
Ivan Baksheev <zpix-dev@list.ru>
2023-03-29 00:03:41 +0700

    fix: loader color changed

--- src/components/ui.tsx
+++ src/components/ui.tsx
@@ -12,21 +12,21 @@ export const ResultsTableLoader = () => {
   return (
     <div className="flex animate-pulse space-x-4">
       <div className="flex-1 space-y-3 py-1">
-        <div className="h-4 rounded bg-slate-700"></div>
+        <div className="h-4 rounded bg-sky-700"></div>
         <div className="space-y-3">
           {Array.from({ length: 10 }).map(() => (
             <div className="grid grid-cols-12 gap-4">
-              <div className="col-span-2 h-2 rounded bg-slate-700"></div>
-              <div className="col-span-1 h-2 rounded bg-slate-700"></div>
-              <div className="col-span-1 h-2 rounded bg-slate-700"></div>
-              <div className="col-span-1 h-2 rounded bg-slate-700"></div>
-              <div className="col-span-1 h-2 rounded bg-slate-700"></div>
-              <div className="col-span-1 h-2 rounded bg-slate-700"></div>
-              <div className="col-span-1 h-2 rounded bg-slate-700"></div>
-              <div className="col-span-1 h-2 rounded bg-slate-700"></div>
-              <div className="col-span-1 h-2 rounded bg-slate-700"></div>
-              <div className="col-span-1 h-2 rounded bg-slate-700"></div>
-              <div className="col-span-1 h-2 rounded bg-slate-700"></div>
+              <div className="col-span-2 h-2 rounded bg-sky-700"></div>
+              <div className="col-span-1 h-2 rounded bg-sky-700"></div>
+              <div className="col-span-1 h-2 rounded bg-sky-700"></div>
+              <div className="col-span-1 h-2 rounded bg-sky-700"></div>
+              <div className="col-span-1 h-2 rounded bg-sky-700"></div>
+              <div className="col-span-1 h-2 rounded bg-sky-700"></div>
+              <div className="col-span-1 h-2 rounded bg-sky-700"></div>
+              <div className="col-span-1 h-2 rounded bg-sky-700"></div>
+              <div className="col-span-1 h-2 rounded bg-sky-700"></div>
+              <div className="col-span-1 h-2 rounded bg-sky-700"></div>
+              <div className="col-span-1 h-2 rounded bg-sky-700"></div>
             </div>
           ))}
         </div>
